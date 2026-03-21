0baee9f27f51ce5ae826c24784aa2a98807b6e5d
Pvr1978 <144450961+Pvr1978@users.noreply.github.com>
2026-03-21 13:59:10 +0100

    Create orderbook.js

---
+++ nodejs-scripts/orderbook.js
@@ -0,0 +1,14 @@
+name: Node.js Orderbook
+
+on:
+  workflow_dispatch:
+
+jobs:
+  orderbook:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v4
+      - uses: actions/setup-node@v4
+        with:
+          node-version: '20'
+      - run: node nodejs-scripts/orderbook.js
